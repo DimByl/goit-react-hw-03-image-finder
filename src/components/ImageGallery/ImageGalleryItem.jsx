0aa30764baca963bc64ad393943eab5e62cfdc70
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-09-30 00:20:28 +0300

    up5

--- src/components/ImageGallery/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -1,24 +1,21 @@
 import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.scss';
 
-const ImageGalleryItem = ({ url, tags, largeImageURL, onClick }) => {
-  return (
-    <li className={styles.ImageGalleryItem} onClick={() => onClick(modalUrl)}>
-      <img
-        src={url}
-        alt={tags}
-        className={styles.ImageGalleryItem__image}
-        data-large-image={largeImageURL}
-      />
-    </li>
-  );
-};
+const ImageGalleryItem = ({ url, tags, largeImageURL }) => (
+  <li className={styles.ImageGalleryItem}>
+    <img
+      src={url}
+      alt={tags}
+      className={styles.ImageGalleryItem__image}
+      data-large-image={largeImageURL}
+    />
+  </li>
+);
 
 ImageGalleryItem.propTypes = {
   webformatURL: PropTypes.string,
   tags: PropTypes.string.isRequired,
   largeImageURL: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
 };
 
 export default ImageGalleryItem;
--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -1,18 +1,13 @@
-const API_BASE = {
-  URL: "https://pixabay.com/api",
-  KEY: "22552656-a52d92f58f7bf96e8efb5df9f",
-  PER_PAGE: 12,
-};
+import axios from "axios";
 
-const fetchGetImages = ({ query, page }) => {
-  return fetch(
-    `${API_BASE.URL}?q=${query}&page=${page}&key=${API_BASE.KEY}&image_type=photo&orientation=horizontal&per_page=${API_BASE.PER_PAGE}`
-  ).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(new Error(`Something went wrong...`));
-  });
-};
+const API_KEY = "22552656-a52d92f58f7bf96e8efb5df9f";
+const BASE_URL = "https://pixabay.com/api/";
 
-export default fetchGetImages;
+const fetchGetImages = ({ searchQuery = "", currentPage = 1, pageSize = 12 }) =>
+  axios
+    .get(
+      `${BASE_URL}?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${pageSize}`
+    )
+    .then((responce) => responce.data);
+
+export default { fetchGetImages };
